3fe83ae73d16d0ed3ac9a32b47c235d83945f723
TheCricket <bcord@me.com>
2018-12-19 04:56:41 -0500

    Untrack Secret so I can keep my token in there.

--- src/main/java/io/chirpbot/twitchdev/secret/Secret.java
+++
@@ -1,8 +0,0 @@
-package io.chirpbot.twitchdev.secret;
-
-public class Secret {
-
-	public static final String token = "";
-	public static final String AWS_ACCESS_KEY = "";
-	public static final String AWS_SECRET_KEY = "";
-}
