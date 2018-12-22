ba8b024278db0b17d809127731072dd7ba5734f4
lclc98 <liam.c14785@gmail.com>
2018-12-22 17:25:34 +1000

    Switched to timer

--- src/main/java/io/chirpbot/twitchdev/handlers/ReadyHandler.java
+++ src/main/java/io/chirpbot/twitchdev/handlers/ReadyHandler.java
@@ -5,12 +5,22 @@ import io.chirpbot.twitchdev.twitch.StreamAnnouncementThread;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ReadyHandler {
 
 	@EventSubscriber
 	public void onReady(ReadyEvent event) {
 		BlogThread blogThread = new BlogThread();
-		//blogThread.start();
+
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				blogThread.run();
+			}
+		}, 60 * 60 * 1000);
 
 		StreamAnnouncementThread streamAnnouncementThread = new StreamAnnouncementThread();
 		streamAnnouncementThread.start();
