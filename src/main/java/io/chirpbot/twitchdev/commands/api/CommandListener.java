419ed67f3d1ac7ed96c27a96e827da6d1ed49c11
Damian Staszewski <damian@stachuofficial.tv>
2019-01-17 17:25:29 +0100

    made CommandListener as abstract class

--- src/main/java/io/chirpbot/twitchdev/commands/api/CommandListener.java
+++ src/main/java/io/chirpbot/twitchdev/commands/api/CommandListener.java
@@ -2,7 +2,7 @@ package io.chirpbot.twitchdev.commands.api;
 
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 
-public class CommandListener {
+public abstract class CommandListener {
     void onCommandUsage(CommandEvent event, Command command) {}
     void onCommandException(CommandEvent event, Command command, Throwable exception) {}
     void onOrdinalMessage(MessageReceivedEvent event) {}
