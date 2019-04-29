8205ee33137062ccd9463bbc0bee24defc64c60d
TheCricket <bcord@me.com>
2019-04-29 16:06:45 -0400

    Fix the timer.

--- modules/RSSFeeds.js
+++ modules/RSSFeeds.js
@@ -17,16 +17,20 @@ exports.init = function(client) {
 exports.parseBlog = function(client) {
     let parser = new RSSParser();
     parser.parseURL('http://blog.twitch.tv/feed', function(err, feed) {
-        if(lastBlogPost == null) {
-            //Startup
-            lastBlogPost = feed.items[0];
-        } else if(lastBlogPost.title === feed.items[0].title) {
-            //Same post
-            client.logger.log('Checked blog posts but found the same one');
+        if(err) {
+            console.log(err);
         } else {
-            //Send message to announcements
-            lastBlogPost = feed.items[0];
-            client.channels.get(`${process.env.ENV === 'development' ? botTesting : announcements}`).send(EmbedBuilder.buildRichEmbed(lastBlogPost.link, lastBlogPost.title, '', lastBlogPost.creator, '', '', 'Brought to you by blog.twitch.tv'));
+            if (lastBlogPost == null) {
+                //Startup
+                lastBlogPost = feed.items[0];
+            } else if (lastBlogPost.title === feed.items[0].title) {
+                //Same post
+                client.logger.log('Checked blog posts but found the same one');
+            } else {
+                //Send message to announcements
+                lastBlogPost = feed.items[0];
+                client.channels.get(`${process.env.ENV === 'development' ? botTesting : announcements}`).send(EmbedBuilder.buildRichEmbed(lastBlogPost.link, lastBlogPost.title, '', lastBlogPost.creator, '', '', 'Brought to you by blog.twitch.tv'));
+            }
         }
     });
 };
@@ -34,16 +38,20 @@ exports.parseBlog = function(client) {
 exports.parseForums = function(client) {
     let par = new RSSParser();
     par.parseURL('https://discuss.dev.twitch.tv/c/announcements.rss', function(err, feed) {
-        if(lastForumPost == null) {
-            //Startup
-            lastForumPost = feed.items[0];
-        } else if(lastForumPost.title === feed.items[0].title) {
-            //Same post
-            client.logger.log('Checked forum posts but found the same one');
+        if(err) {
+            console.log(err);
         } else {
-            //Send message to announcements
-            lastForumPost = feed.items[0];
-            client.channels.get(`${process.env.ENV === 'development' ? botTesting : announcements}`).send(EmbedBuilder.buildRichEmbed(lastForumPost.link, lastForumPost.title, lastForumPost.contentSnippet.substr(0, 237) + '...', lastForumPost.creator, '', '', 'Brought to you by the dev forums'));
+            if (lastForumPost == null) {
+                //Startup
+                lastForumPost = feed.items[0];
+            } else if (lastForumPost.title === feed.items[0].title) {
+                //Same post
+                client.logger.log('Checked forum posts but found the same one');
+            } else {
+                //Send message to announcements
+                lastForumPost = feed.items[0];
+                client.channels.get(`${process.env.ENV === 'development' ? botTesting : announcements}`).send(EmbedBuilder.buildRichEmbed(lastForumPost.link, lastForumPost.title, lastForumPost.contentSnippet.substr(0, 237) + '...', lastForumPost.creator, '', '', 'Brought to you by the dev forums'));
+            }
         }
     });
 };
@@ -51,19 +59,23 @@ exports.parseForums = function(client) {
 exports.parseStatus = function(client) {
     let p = new RSSParser();
     p.parseURL('https://devstatus.twitch.tv/history.rss', function(err, feed) {
-        if(lastStatusPost == null) {
-            //Startup
-            lastStatusPost = feed.items[0];
-        } else if(lastStatusPost.title === feed.items[0].title) {
-            client.logger.log('Checked status update posts but found the same one');
+        if(err) {
+            console.log(err);
         } else {
-            lastStatusPost = feed.items[0];
-            client.channels.get(`${process.env.ENV === 'development' ? botTesting : announcements}`).send(EmbedBuilder.buildRichEmbed(lastStatusPost.link, lastStatusPost.title, lastStatusPost.contentSnippet.substr(0, 237) + '...', 'TwitchDev', '', '', 'Brought to you by dev.twitch.tv/status'));
+            if (lastStatusPost == null) {
+                //Startup
+                lastStatusPost = feed.items[0];
+            } else if (lastStatusPost.title === feed.items[0].title) {
+                client.logger.log('Checked status update posts but found the same one');
+            } else {
+                lastStatusPost = feed.items[0];
+                client.channels.get(`${process.env.ENV === 'development' ? botTesting : announcements}`).send(EmbedBuilder.buildRichEmbed(lastStatusPost.link, lastStatusPost.title, lastStatusPost.contentSnippet.substr(0, 237) + '...', 'TwitchDev', '', '', 'Brought to you by dev.twitch.tv/status'));
+            }
         }
     });
 };
 
 exports.listen = function(client) {
-    this.init(client);
-    setInterval(function() {this.init(client);}, 600000);
+    exports.init(client);
+    setInterval(function() {exports.init(client);}, 60000);
 };
