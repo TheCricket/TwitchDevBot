9f0351a0ffd9088c58405fb39588ace0774869eb
TheCricket <bcord@me.com>
2019-06-13 17:34:26 -0400

    Finish TwitchLink

--- commands/link.js
+++ commands/link.js
@@ -1,21 +1,99 @@
 const Categories = require('../utils/Categories');
-const TwitchAPI = require('../utils/TwitchAPI');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+const axios = require('axios');
 
-exports.run = async (client, message, args) => {
-    if(args != null) {
-        switch(args[0]) {
-            case 'game':
-                message.author.createDM().then((channel) => {
-                    channel.send(TwitchAPI.fetchAuthURLForGames(message.author.id))
-                });
-                break;
-            case 'extension':
-                message.author.createDM().then((channel) => {
-                    channel.send(TwitchAPI.fetchAuthURLForExtensions(message.author.id))
-                });
-                break;
+let id = '';
+let client = {};
+
+const app = express();
+const port = 3000;
+app.use(session({secret: process.env.SESSION_SECRET}));
+app.use(passport.initialize());
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.use('twitch', new OAuth2Strategy({
+    authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+    tokenURL: 'https://id.twitch.tv/oauth2/token',
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.REDIRECT_URI,
+    state: id
+}, (accessToken, refreshToken, profile, done) => {
+    profile.accessToken = accessToken;
+    profile.refreshToken = refreshToken;
+    done(null, profile);
+}));
+
+app.get('/auth/twitch', passport.authenticate('twitch', {scope: process.env.SCOPE}));
+app.get('/auth/twitch/callback', passport.authenticate('twitch', {successRedirect: `/auth/success`, failureRediredct: '/auth/failure'}));
+app.get('/auth/success', (req, res) => {
+    const config = {
+        headers: {
+            'Authorization': `Bearer ${req.session.passport.user.accessToken}`
         }
-    }
+    };
+    axios.get('https://api.twitch.tv/helix/analytics/extensions', config).then((response) => {
+        response.acknowledge();
+        if(response.status === 200) {
+            console.log(id);
+            client.fetchUser(id).then((user) => {
+                console.log(`Adding role to ${user.tag}`);
+                client.guilds.last().fetchMember(user).then((member) => {
+                    client.guilds.last().roles.forEach(role => {
+                        if(role.name === 'Extension Developer') {
+                            member.addRole(role.id);
+                        }
+                    });
+                });
+            });
+        }
+    }).catch((error) => {
+
+    });
+
+    axios.get('https://api.twitch.tv/helix/analytics/games', config).then((response) => {
+        response.acknowledge();
+        if(response.status === 200) {
+            console.log(id);
+            client.fetchUser(id).then((user) => {
+                console.log(`Adding role to ${user.tag}`);
+                client.guilds.last().fetchMember(user).then((member) => {
+                    client.guilds.last().roles.forEach(role => {
+                        if(role.name === 'Game Developer') {
+                            member.addRole(role.id);
+                        }
+                    });
+                });
+            });
+        }
+    }).catch((error) => {
+
+    });
+    res.redirect('http://link.twitch.tv/devchat');
+});
+
+app.get('/auth/failure', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`TwitchDevBot listening on port ${port}!`));
+
+
+exports.run = async (c, message, args) => {
+    client = c;
+    message.author.createDM().then((channel) => {
+        id = message.author.id;
+        channel.send('http://217beb77.ngrok.io/auth/twitch');
+    });
 };
 
 exports.conf = {
--- index.js
+++ index.js
@@ -4,16 +4,8 @@ const Discord = require('discord.js');
 const modules = require('./modules/Logger');
 const RSSFeeds = require('./modules/RSSFeeds');
 const functions = require('./modules/functions');
-const TwitchAPI = require('./utils/TwitchAPI');
-const axios = require('axios');
-const express = require('express');
-const router = express.Router();
 const Enmap = require('enmap');
 require('dotenv').config();
-
-const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`TwitchDevBot listening on port ${port}!`));
 
 const client = new Discord.Client();
 client.logger = modules;
@@ -22,57 +14,6 @@ client.commands = new Enmap();
 client.aliases = new Enmap();
 
 console.log(`We are in ${process.env.ENV}`);
-
-app.get('/auth', (req, res) => {
-    if(req.query.code !== null) {
-        const url = TwitchAPI.fetchAccessTokenURL(req.query.code, req.query.state);
-        axios.post(url);
-        res.redirect('https://link.twitch.tv/devchat');
-    } else {
-        const json = req.body;
-        const access_token = json.access_token;
-        const config = {
-            headers: {
-                'Authorization': `Bearer ${access_token}`
-            }
-        };
-        const type = req.query.state.split('+')[0];
-        const discordID = req.query.state.split('+')[1];
-        if (type === 'extensions') {
-            axios.get('https://api.twitch.tv/helix/analytics/extensions', config).then((response) => {
-                const json = JSON.parse(response.data);
-                if (json.data !== []) {
-                    client.fetchUser(discordID).then((user) => {
-                        logger.log(`Adding role to ${user.tag}`);
-                        client.guilds.last().fetchMember(user).then((member) => {
-                            client.guilds.last().roles.forEach(role => {
-                                if(role.name === 'Extension Developer') {
-                                    member.addRole(role.id);
-                                }
-                            });
-                        });
-                    });
-                }
-            });
-        } else {
-            axios.get('https://api.twitch.tv/helix/analytics/games', config).then((response) => {
-                const json = JSON.parse(response.data);
-                if (json.data !== []) {
-                    client.fetchUser(discordID).then((user) => {
-                        logger.log(`Adding role to ${user.tag}`);
-                        client.guilds.last().fetchMember(user).then((member) => {
-                            client.guilds.last().roles.forEach(role => {
-                                if(role.name === 'Game Developer') {
-                                    member.addRole(role.id);
-                                }
-                            });
-                        });
-                    });
-                }
-            });
-        }
-    }
-});
 
 const init = async () => {
     const cmdFiles = await readdir('./commands/');
