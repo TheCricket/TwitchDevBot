8e48b01d3603653c823fb14ad43a967c3ff25a24
Barry Carlyon <barry@barrycarlyon.co.uk>
2020-07-27 15:05:50 +0100

    Add a !libraries (aliased to !libs) for easy routing to the librarys/Twitch Libraries Chat Discord

---
+++ commands/commands/libraries.js
@@ -0,0 +1,19 @@
+const Categories = require('../../core/utils/Categories');
+
+exports.run = async (client, message, args) => {
+    message.channel.send(`Hey there! Looks like you are asking questions about a specific Twitch Library! You might want to join the "Twitch Libraries Chat" Discord for Focussed help https://discord.gg/8NXaEyV`);
+};
+
+exports.conf = {
+    enabled: true,
+    aliases: [],
+    ranks: [],
+};
+
+exports.help = {
+    name: 'libraries',
+    category: Categories.HELP,
+    description: `Questions about a specific Twitch Library? This command will point you in the right direction!`,
+    usage: '!libraries',
+    aliases: ['!libs']
+};
